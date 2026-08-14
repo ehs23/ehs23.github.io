02bb3c0e6fa5074b2344a83366eb29551c55b8e5
ehs23 <ehshan1023@gmail.com>
2026-08-15 03:10:46 +0900

    gifgit status

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,9 @@ import { getAllPosts } from "@/lib/posts";
 // 방금 만든 게시글 목록 컴포넌트
 import PostFeed from "@/components/PostFeed";
 
+import Image from "next/image";
+import giphy from "@/components/giphy.gif";
+
 export const metadata: Metadata = {
   alternates: {
     canonical: "/",
@@ -23,17 +26,19 @@ export default function HomePage() {
 
       {/* 블로그 상단 영역 */}
       <header className="home-header">
+        <Image
+          src={giphy}
+          alt="TAF 캐릭터"
+          className="blog-title-gif"
+          unoptimized
+          priority
+        />
 
-        {/* 블로그 제목 */}
-        <h1 className="blog-name">
-          TAF : ehs23
-        </h1>
+        <h1 className="blog-name">TAF : ehs23</h1>
 
-        {/* 블로그에 대한 간단한 설명 */}
         <p className="blog-description">
           나의 개발 및 학습을 기록하도록 만든 자작 사이트입니다.
         </p>
-
       </header>
 
 
